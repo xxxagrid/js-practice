66bff531b3c6d25c2724eb2db710cf8fbebf621d
Victoria <baranvika53@gmail.com>
2023-01-31 13:32:39 +0200

    color for  tag p, show the values of attributes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
 "use strict"
 
-function getFullName (){
-    const formItems = document.getElementById('form1');
+//TASK 1
+// function getFullName (){
+//     const formItems = document.getElementById('form1');
 
-    for(let i = 0; i < formItems.length; i++){
-        if(formItems[i].value != 'Submit'){
-            console.log(formItems[i].value);
-        }
+//     for(let i = 0; i < formItems.length; i++){
+//         if(formItems[i].value != 'Submit'){
+//             console.log(formItems[i].value);
+//         }
+//     }
+// }
+// getFullName();
+
+//TASK 2
+//Set background
+const paragraphItem = document.getElementById('parag')
+const linkItem = document.getElementById('w3r');
+function setBackground(){
+    // paragraphItem.classList.add('orange');
+    paragraphItem.setAttribute('style', 'background-color: orange');
+}
+paragraphItem.addEventListener('mouseover', setBackground);
+
+//Show all the attributes
+function getAttributes(){
+    let attributes = linkItem.attributes;
+    console.log(attributes);
+    for(let val in attributes){
+        console.log(`Value: ${attributes[val].value}`);
     }
 }
-getFullName();
