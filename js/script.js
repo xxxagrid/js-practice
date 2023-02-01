3eee4c3c34c70d3e94e06fe38a292e17aa66609a
Victoria <baranvika53@gmail.com>
2023-02-01 12:22:05 +0200

    function which create new row with 2 cells

--- js/script.js
+++ js/script.js
@@ -14,19 +14,32 @@
 
 //TASK 2
 //Set background
-const paragraphItem = document.getElementById('parag')
-const linkItem = document.getElementById('w3r');
-function setBackground(){
-    // paragraphItem.classList.add('orange');
-    paragraphItem.setAttribute('style', 'background-color: orange');
-}
-paragraphItem.addEventListener('mouseover', setBackground);
+// const paragraphItem = document.getElementById('parag')
+// const linkItem = document.getElementById('w3r');
+// function setBackground(){
+//     // paragraphItem.classList.add('orange');
+//     paragraphItem.setAttribute('style', 'background-color: orange');
+// }
+// paragraphItem.addEventListener('mouseover', setBackground);
 
-//Show all the attributes
-function getAttributes(){
-    let attributes = linkItem.attributes;
-    console.log(attributes);
-    for(let val in attributes){
-        console.log(`Value: ${attributes[val].value}`);
-    }
+// //Show all the attributes
+// function getAttributes(){
+//     let attributes = linkItem.attributes;
+//     console.log(attributes);
+//     for(let val in attributes){
+//         console.log(`Value: ${attributes[val].value}`);
+//     }
+// }
+
+//TASK 3
+
+const tableItem = document.getElementById('sampleTable');
+let rowCount = 3;
+tableItem.addEventListener('click', insert_Row);
+
+function insert_Row(){
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `<td>Row${rowCount} cell1</td> <td>Row${rowCount} cell2</td>`;
+    tableItem.append(newRow);
+    rowCount++;
 }
